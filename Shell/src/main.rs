351d95f6c433fbcea96591b1aa847ec6cc00931a
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-09-27 23:53:29 +0330

    Add: std:env

--- Shell/src/main.rs
+++ Shell/src/main.rs
@@ -2,12 +2,16 @@ use std::io;
 
 fn main(){
 
+    let args: Vec<String> = std::env::args().collect(); 
     loop {
         let mut line = String::new();
         io::stdin().read_line(&mut line).unwrap();
+        let line = line.trim();
         println!( "{}" , line);
     }
 }
+
+
 
 
  
