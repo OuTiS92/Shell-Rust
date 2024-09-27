4a5903429c58fd25dbb1657baf3518b8d4e5a360
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-09-27 17:46:55 +0330

    Add: loop functions

--- Shell/src/main.rs
+++ Shell/src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
-use::std::fs;
-
+use std::io;
 
 fn main(){
 
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        println!( "{}" , line);
+    }
 }
 
 
