b499bba7f231e0ea56d1c2c1d6100cc6ecf4f267
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-10-08 22:43:07 +0330

    unsafe

--- Shell/src/main.rs
+++ Shell/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
+use std::ffi::CString;
 use std::process::Command;
 use std::{fmt::Error, io, path::PathBuf};
 use std::path::{self, Path};
 
-use libc::ENETUNREACH;
+use libc::{c_char, ENETUNREACH};
 
 fn main(){
 
@@ -20,14 +21,25 @@ fn main(){
 
 fn run_process(vars: &HashMap<String,String> , commnad: &str) -> Result< () , () > {
     let command : Vec<&str> = commnad.split(" ").collect();
-    let bin = find_binary(commnad, &vars["PATH"]);
+    let bin =  match find_binary(commnad[0], &vars["PATH"]){
+        Ok(b) => b , 
+        Err(err) => {
+            println!("Failed to find {}" , command[0]);
+            return Err(());
+        }
+    };
 
+    use std::os::raw::c_char;
     match  unsafe { libc::fork()} {
         -1 => {
             panic!("failed to start child process");
         }
         0 => {
-             unsafe {};
+            let pathname = &bin;
+            let argv_owned : Vec<CString> = command.iter().map(|p| CString::new(*p).unwrap()).collect();
+            let argv : Vec <*const c_char > = argv_owned.iter().map(|o| o.as_ptr()).collect();
+            let argv: *const *const c_char = argv.as_ptr();
+            unsafe {};
         }
         child_pid =>{
             println!( "hello chid is {child_pid} ");
